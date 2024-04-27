2cea64bf49e8c14a1814707d9c801a5eebc32d56
Jad-Doggy <pandajadon@gmail.com>
2024-04-27 14:07:31 -0700

    boos

--- Assets/GunderlandImports/pointManager.cs
+++ Assets/GunderlandImports/pointManager.cs
@@ -7,6 +7,7 @@ public class pointManager : MonoBehaviour
 {
     public static pointManager PM_Instance;
     public int totalPoints;
+    public int numIncorrect;
     [SerializeField] TMP_Text score;
 
     private void Awake()
@@ -21,10 +22,28 @@ public class pointManager : MonoBehaviour
     private void Start()
     {
         totalPoints = 0;
+        numIncorrect = 0;
     }
 
     public void GainPoint(int points)
     {
+        if (points <= 0)
+        {
+            ++numIncorrect;
+        } else if (points > 0)
+        {
+            numIncorrect = 0;
+        }
+
+        if (numIncorrect >= 3)
+        {
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName("playersucks", 1);
+        } else 
+        {
+            FMODUnity.RuntimeManager.StudioSystem.setParameterByName("playersucks", 0);
+        }
+            
+        
         int pointsGained = (int)(points * comboManager.CM_Instance.currentMultiplier);
         totalPoints += pointsGained;
         comboManager.CM_Instance.GainComboPoints(pointsGained);
