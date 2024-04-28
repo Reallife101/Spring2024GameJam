bfca09943699dc3baa96a2b876152de686f62c48
Funbam <bambamtheram@gmail.com>
2024-04-28 15:18:12 -0700

    Stream end

--- Assets/Prefabs/New Folder/StreamEnd.cs
+++ Assets/Prefabs/New Folder/StreamEnd.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StreamEnd : MonoBehaviour
 {
     [SerializeField] Animator animator;
     [SerializeField] private float timeUntilStreamEnd;
     [SerializeField] private TMP_Text timeLeftText;
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private string mainMenuName;
     void Start()
     {
         
@@ -20,6 +23,7 @@ public class StreamEnd : MonoBehaviour
         timeLeftText.text = ((int)timeUntilStreamEnd).ToString();
         if(timeUntilStreamEnd < 0)
         {
+            scoreText.text = "Final Viewer Count: " + pointManager.PM_Instance.totalPoints.ToString();
             animator.SetTrigger("End");
             FMODUnity.RuntimeManager.StudioSystem.setParameterByName("win", 1);
         }
@@ -27,5 +31,15 @@ public class StreamEnd : MonoBehaviour
         {
             Time.timeScale = 0;
         }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().ToString());
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(mainMenuName);
     }
 }
